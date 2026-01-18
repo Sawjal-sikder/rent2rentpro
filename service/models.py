3b85c5e67ee6aa7ddf857dc9310d8983f5ab8cb2
sajal <mdsajalsikder@gmail.com>
2026-01-18 18:39:49 +0600

    feat: Add ContactCreationFile model for managing contract files with timestamps

--- service/models.py
+++ service/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+class ContactCreationFile(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='contracts/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
